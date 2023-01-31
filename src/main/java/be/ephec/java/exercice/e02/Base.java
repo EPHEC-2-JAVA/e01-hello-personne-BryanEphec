157f4e60d8e0332e427eb7d1373e8dd974a98855
BryanTheKing <b.g.c@hotmail.be>
2023-01-31 15:37:02 +0100

    exercice 2 mais pas reussi l'affichage alphabetique de la base 16

---
+++ src/main/java/be/ephec/java/exercice/e02/Base.java
@@ -0,0 +1,22 @@
+package be.ephec.java.exercice.e02;
+
+import java.util.Scanner;
+
+public class Base {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int base,number;
+        do {
+            System.out.print("Enter the base and number (0 0 to quit): ");
+            base = input.nextInt();
+            number = input.nextInt();
+
+            if (base != 0 && number != 0) {
+                String numberString = Integer.toString(number);
+                int result = Integer.parseInt(numberString, base);
+                System.out.println("The number in base"+ base +" is: " + result);
+            }
+        } while (base != 0 && number != 0);
+
+    }
+}
