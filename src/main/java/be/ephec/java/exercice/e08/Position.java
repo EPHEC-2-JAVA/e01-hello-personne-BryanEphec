c7e80106d939629fc0b1944eae3d6be06685e5c0
BryanTheKing <b.g.c@hotmail.be>
2023-02-08 11:00:37 +0100

    new dossier et fichier

--- src/main/java/be/ephec/java/exercice/e08/Position.java
+++ src/main/java/be/ephec/java/exercice/e08/Position.java
@@ -1,4 +1,11 @@
 package be.ephec.java.exercice.e08;
 
 public class Position {
+    double x;
+    double y;
+
+    public Position(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
 }
--- src/main/java/be/ephec/java/exercice/e08/Shape.java
+++ src/main/java/be/ephec/java/exercice/e08/Shape.java
@@ -1,5 +1,9 @@
 package be.ephec.java.exercice.e08;
 
 public abstract class Shape {
+        public double rotation;
 
+        public abstract void affiche();
+
+        public abstract void effacer();
 }
